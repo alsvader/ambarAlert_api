947c8dc3a2a37e5f725d0b5048c243952756deea
alsvader <alsvader@hotmail.com>
2020-01-31 00:21:39 -0600

    Added swagger-ui-express to generated API documentation, and configure the basic folder structure for the project

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,6 +18,7 @@ module.exports = {
     'linebreak-style': ['error', 'unix'],
     quotes: ['error', 'single'],
     semi: ['error', 'always'],
+    'no-console': 'off',
     'prettier/prettier': 'error'
   }
 };
