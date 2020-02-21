7a32dea75b0a9905b9f75c0e182e8c9ba6f69db6
Aaron Lopez - a0l01ge <Aaron.Lopez0@walmart.com>
2020-02-21 13:24:57 -0600

    Added cors

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ import path from 'path';
 import morgan from 'morgan';
 import swaggerUi from 'swagger-ui-express';
 import swaggerJSDoc from 'swagger-jsdoc';
+import cors from 'cors';
 import swaggerOptions from './swagger-docs';
 import routes from './routes';
 import db from './config/db';
@@ -18,6 +19,7 @@ db.authenticate()
 const server = express();
 server.use(morgan('dev'));
 server.use(express.json());
+server.use(cors);
 server.use('/storage', express.static(path.join(__dirname, 'public/uploads')));
 server.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 server.use('/api', routes);
